0187c93d22fe693ba982d90290eac0690c933574
Ananda Shabrina Putri Gunawan <48620949+nandaisnanda@users.noreply.github.com>
2023-05-28 08:16:44 +0700

    Update index.js

--- joinmun-web/pages/index.js
+++ joinmun-web/pages/index.js
@@ -9,6 +9,21 @@ export default function Home() {
 				<title>JOINMUN 2023</title>
 			</Head>
 			<Hero />
+		<h1 className=" mt-20 text-center font-headline text-6xl">Council and Topic</h1>
+			<h1 className=" text-center font-headline text-6xl">Reveal</h1>
+			<Image 
+			src=""
+			width={500}
+			height={500}
+			/>
+			<div> 
+				<button className="mb-20 mt-20 mr-10 ml-10">
+					Single Delegate Advanced Council 
+				</button>
+				<button className="">
+					Double Delegate Advanced Council 
+				</button>
+			</div>
 		</>
 	);
 }
