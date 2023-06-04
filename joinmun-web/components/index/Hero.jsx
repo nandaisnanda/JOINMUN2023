804787f4429cbd3a46028969f93d9a623ee397e5
David Lois <107032089+DvaDvaD@users.noreply.github.com>
2023-06-04 13:20:52 +0700

    responsive hero

--- joinmun-web/components/index/Hero.jsx
+++ joinmun-web/components/index/Hero.jsx
@@ -8,9 +8,9 @@ import "swiper/css";
 
 const Hero = () => {
 	return (
-		<section className="relative font-headline">
+		<section className="relative mt-20 font-headline lg:mt-0">
 			<Image
-				className="absolute inset-0 m-auto h-auto w-3/5"
+				className="absolute inset-0 m-auto h-auto w-4/5 md:w-3/5"
 				src={kipas}
 				alt="kipas"
 			/>
@@ -30,38 +30,24 @@ const Hero = () => {
 				}}
 				modules={[Navigation, Pagination]}
 			>
-				<SwiperSlide className="min-h-[80vh] px-28 pt-28">
-					<p className="mb-12 text-4xl">GRAND THEME</p>
-					<h2 className="mb-2 text-7xl">Ashes of the Old Order</h2>
-					<p className="mb-12 text-3xl">
-						Emergence of the All-Informed Architects of Modern Politics
-					</p>
-					<button className="rounded-full bg-yellow px-4 py-1 font-body text-lg text-red">
-						more information
-					</button>
-				</SwiperSlide>
-				<SwiperSlide className="min-h-[80vh] px-28 pt-28">
-					<p className="mb-12 text-4xl">GRAND THEME</p>
-					<h2 className="mb-2 text-7xl">Ashes of the Old Order</h2>
-					<p className="mb-12 text-3xl">
-						Emergence of the All-Informed Architects of Modern Politics
-					</p>
-					<button className="rounded-full bg-yellow px-4 py-1 font-body text-lg text-red">
-						more information
-					</button>
-				</SwiperSlide>
-				<SwiperSlide className="min-h-[80vh] px-28 pt-28">
-					<p className="mb-12 text-4xl">GRAND THEME</p>
-					<h2 className="mb-2 text-7xl">Ashes of the Old Order</h2>
-					<p className="mb-12 text-3xl">
-						Emergence of the All-Informed Architects of Modern Politics
-					</p>
-					<button className="rounded-full bg-yellow px-4 py-1 font-body text-lg text-red">
-						more information
-					</button>
-				</SwiperSlide>
-				<BsChevronLeft className="swiper-button-prev absolute inset-y-0 z-10 my-auto h-10 w-10 cursor-pointer fill-yellow" />
-				<BsChevronLeft className="swiper-button-next absolute inset-y-0 right-0 z-10 my-auto h-10 w-10 rotate-180 cursor-pointer fill-yellow" />
+				{headlineList.map((headline) => (
+					<SwiperSlide className="!flex min-h-[80vh] flex-col justify-center px-12 sm:px-20 md:px-28">
+						<p className="mb-4 text-xl uppercase sm:mb-8 sm:text-2xl md:text-3xl lg:text-4xl">
+							{headline.tag}
+						</p>
+						<h2 className="mb-2 text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+							{headline.headline}
+						</h2>
+						<p className="mb-6 text-lg sm:mb-12 sm:text-xl md:text-2xl lg:text-3xl">
+							{headline.description}
+						</p>
+						<button className="w-fit rounded-full bg-yellow px-4 py-1 font-body text-red md:text-lg">
+							more information
+						</button>
+					</SwiperSlide>
+				))}
+				<BsChevronLeft className="swiper-button-prev absolute inset-y-0 z-10 my-auto h-auto w-8 cursor-pointer fill-yellow sm:w-10" />
+				<BsChevronLeft className="swiper-button-next absolute inset-y-0 right-0 z-10 my-auto h-auto w-8 rotate-180 cursor-pointer fill-yellow sm:w-10" />
 				<div className="flex justify-center">
 					<div className="swiper-pagination flex gap-2" />
 				</div>
@@ -70,3 +56,21 @@ const Hero = () => {
 	);
 };
 export default Hero;
+
+const headlineList = [
+	{
+		tag: "Grand Theme",
+		headline: "Ashes of the Old Order",
+		description: "Emergence of the All-Informed Architects of Modern Politics",
+	},
+	{
+		tag: "Grand Theme",
+		headline: "Ashes of the Old Order",
+		description: "Emergence of the All-Informed Architects of Modern Politics",
+	},
+	{
+		tag: "Grand Theme",
+		headline: "Ashes of the Old Order",
+		description: "Emergence of the All-Informed Architects of Modern Politics",
+	},
+];
