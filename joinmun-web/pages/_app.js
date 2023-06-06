fd546600739260fff3eaaf72febd841ca6b39498
easy <izaaz240@gmail.com>
2023-06-06 18:53:23 +0700

    refactor(app): move Head placement to _app.js

--- joinmun-web/pages/_app.js
+++ joinmun-web/pages/_app.js
@@ -2,32 +2,36 @@ import Footer from "@/components/Footer";
 import "@/styles/globals.css";
 import dynamic from "next/dynamic";
 import localFont from "next/font/local";
+import Head from "next/head";
 
 const josefinSans = localFont({
-	src: "./../public/fonts/josefin-sans/JosefinSans-VariableFont_wght.ttf",
-	variable: "--font-josefin-sans",
+  src: "./../public/fonts/josefin-sans/JosefinSans-VariableFont_wght.ttf",
+  variable: "--font-josefin-sans",
 });
 const safiraMarch = localFont({
-	src: "./../public/fonts/safira-march/Safira March.otf",
-	variable: "--font-safira-march",
+  src: "./../public/fonts/safira-march/Safira March.otf",
+  variable: "--font-safira-march",
 });
 const philosopher = localFont({
-	src: "./../public/fonts/philosopher/Philosopher-Regular.ttf",
-	variable: "--font-philosopher",
+  src: "./../public/fonts/philosopher/Philosopher-Regular.ttf",
+  variable: "--font-philosopher",
 });
 
 const Navbar = dynamic(() => import("@/components/Navbar"), { ssr: false });
 
 export default function App({ Component, pageProps }) {
-	return (
-		<main
-			className={`${josefinSans.variable} ${safiraMarch.variable} ${philosopher.variable} bg-red font-body text-yellow`}
-		>
-			<div className="mb-28 min-h-screen pt-20 lg:pt-0">
-				<Navbar />
-				<Component {...pageProps} />
-			</div>
-			<Footer />
-		</main>
-	);
+  return (
+    <main
+      className={`${josefinSans.variable} ${safiraMarch.variable} ${philosopher.variable} bg-red font-body text-yellow`}
+    >
+      <Head>
+        <title>JOINMUN 2023</title>
+      </Head>
+      <div className="mb-28 min-h-screen pt-20 lg:pt-0">
+        <Navbar />
+        <Component {...pageProps} />
+      </div>
+      <Footer />
+    </main>
+  );
 }
--- joinmun-web/pages/index.js
+++ joinmun-web/pages/index.js
@@ -1,39 +1,53 @@
 import Hero from "@/components/index/Hero";
-import Head from "next/head";
 import Image from "next/image";
 
 export default function Home() {
-	return (
-		<>
-			<Head>
-				<title>JOINMUN 2023</title>
-			</Head>
-			<Hero />
-			<div className="bg-cover bg-center bg-opacity-10" style={{ backgroundImage: 'url("/index/Untitled_Artwork-1 10.svg")' }}>
-			<h1 className=" mt-10 text-center font-headline text-6xl">Council and Topic</h1>
-			<h1 className="text-center font-headline text-6xl -mb-32">Reveal</h1>
-			<button className=" absolute bg-yellow hover:bg-sub-yellow text-red font-headline py-2 px-4 rounded-full right-[340px] my-[345px]">
-				<h1>Single Delegate</h1>
-				<h1>Advanced Council</h1>
-			</button>
-			<button className="absolute bg-yellow hover:bg-sub-yellow text-red font-headline py-2 px-4 rounded-full left-[340px] my-[345px]">
-				<h1>Double Delegates</h1>
-				<h1>Advanced Council</h1>
-			</button>
-			<div className="flex justify-center">
-			<Image
-			src="/index/jogja silhouette .png"
-			width={800}
-			height={800}
-			/> 
-			</div> 
-			</div>
-			<div className=" bg-cover bg-center bg-opacity-10 " style={{ backgroundImage: 'url("/index/Untitled_Artwork-1 11 (1).svg")' }}>
-				<h1 className="font-headline mt-20 text-6xl text-center">Exhibition (coming soon)</h1>
-			</div >
-			<div className="bg-cover bg-center bg-opacity-10 " style={{ backgroundImage: 'url("/index/Untitled_Artwork-1 11 (1).svg")' }}>
-				<h1 className="font-headline mt-20 text-6xl text-center">Gallery</h1>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <Hero />
+      <div
+        className="bg-opacity-10 bg-cover bg-center"
+        style={{ backgroundImage: 'url("/index/Untitled_Artwork-1 10.svg")' }}
+      >
+        <h1 className=" mt-10 text-center font-headline text-6xl">
+          Council and Topic
+        </h1>
+        <h1 className="-mb-32 text-center font-headline text-6xl">Reveal</h1>
+        <button className=" absolute right-[340px] my-[345px] rounded-full bg-yellow px-4 py-2 font-headline text-red hover:bg-sub-yellow">
+          <h1>Single Delegate</h1>
+          <h1>Advanced Council</h1>
+        </button>
+        <button className="absolute left-[340px] my-[345px] rounded-full bg-yellow px-4 py-2 font-headline text-red hover:bg-sub-yellow">
+          <h1>Double Delegates</h1>
+          <h1>Advanced Council</h1>
+        </button>
+        <div className="flex justify-center">
+          <Image
+            src="/index/jogja silhouette .png"
+            width={800}
+            height={800}
+            alt="jogja silhoutte"
+          />
+        </div>
+      </div>
+      <div
+        className=" bg-opacity-10 bg-cover bg-center "
+        style={{
+          backgroundImage: 'url("/index/Untitled_Artwork-1 11 (1).svg")',
+        }}
+      >
+        <h1 className="mt-20 text-center font-headline text-6xl">
+          Exhibition (coming soon)
+        </h1>
+      </div>
+      <div
+        className="bg-opacity-10 bg-cover bg-center "
+        style={{
+          backgroundImage: 'url("/index/Untitled_Artwork-1 11 (1).svg")',
+        }}
+      >
+        <h1 className="mt-20 text-center font-headline text-6xl">Gallery</h1>
+      </div>
+    </>
+  );
 }
