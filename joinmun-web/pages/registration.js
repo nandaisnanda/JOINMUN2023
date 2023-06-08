5af04ab39fa02a904a08e370fd5fc2c5b9d5a7e8
David Lois <107032089+DvaDvaD@users.noreply.github.com>
2023-06-08 14:39:22 +0700

    fix registration form margin

--- joinmun-web/pages/registration.js
+++ joinmun-web/pages/registration.js
@@ -1,10 +1,10 @@
 const Registration = () => {
 	return (
-		<div className="-mb-28">
+		<div>
 			<iframe
 				title="Google Form Embed"
 				id="google-form-iframe"
-				height="1250"
+				height="900"
 				className="container mx-auto !overflow-visible md:px-16 lg:px-24"
 				src="https://docs.google.com/forms/d/e/1FAIpQLScFF0zJUSuIiErUlxVlP10Zxlcn4tLuEFEqoDHFNScV4OQs_Q/viewform"
 				frameborder="0"
