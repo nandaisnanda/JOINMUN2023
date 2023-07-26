f0cad0ab6cbb287cfddc1c021118487eb61daaa1
BagusAlwan <bgslwn28@gmail.com>
2023-07-26 21:31:53 +0700

    Change the iframe link

--- joinmun-web/pages/registration.js
+++ joinmun-web/pages/registration.js
@@ -11,7 +11,7 @@ const Registration = () => {
 				id="google-form-iframe"
 				height="900"
 				className="container mx-auto !overflow-visible md:px-16 lg:px-24"
-				src="https://docs.google.com/forms/d/e/1FAIpQLScFF0zJUSuIiErUlxVlP10Zxlcn4tLuEFEqoDHFNScV4OQs_Q/viewform"
+				src="https://bit.ly/LateWaveRegistration"
 				frameborder="0"
 			></iframe>
 		</div>
