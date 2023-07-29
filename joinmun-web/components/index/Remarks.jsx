714c336aaf88f5f4504d4f348bd94f991af5d909
DvaDvaD <davidlou0810@gmail.com>
2023-07-29 18:41:08 +0700

    reveal page animation

--- joinmun-web/components/index/Remarks.jsx
+++ joinmun-web/components/index/Remarks.jsx
@@ -2,52 +2,56 @@ import Image from "next/image";
 import fotoPresident from "@/public/index/foto-president.jpg";
 
 const Remarks = () => {
-	return (
-		<section className="container mx-auto mb-28 space-y-8 px-8 sm:px-12 lg:px-16">
-			<div className="flex flex-col items-center justify-center space-y-6 font-headline md:flex-row md:space-x-10 md:space-y-0">
-				<Image
-					className="h-auto w-36 rounded-full shadow-[0_0_10px_white] md:w-44 lg:w-56"
-					src={fotoPresident}
-					alt="foto president"
-				/>
-				<div className="text-center md:space-y-4 md:text-left">
-					<h2 className="text-xl font-bold text-[#f4bb9c] sm:text-2xl md:text-3xl lg:text-4xl">
-						Muhammad Riefky Fahreza
-					</h2>
-					<p className="text-base md:text-lg lg:text-xl">
-						President of Jogja International MUN 2023 <br /> Faculty of Social
-						and Political Science, <br /> Universitas Gadjah Mada
-					</p>
-				</div>
-			</div>
-			<p className="mx-auto max-w-prose text-justify">
-				It is beyond my honor to announce the establishment of the 12th
-				conference of Jogja International Model United Nations (‘JOINMUN’), one
-				of the founding MUNs that contains the legacy of great MUNers of
-				Indonesia. The 12th JOINMUN is scheduled to take place from 28 September
-				To 1 October 2023. In this year also, we are committed to providing a
-				platform for insightful discussions and thought-provoking inquiries into
-				current national, regional, and international events. <br /> <br /> We
-				note with regret that these discussions had been pushed back whilst the
-				world faces a multitude of challenges, including high inflation rates,
-				the COVID-19 pandemic, and climate change. The recent developments in
-				Ukraine, with Russia's conquest, have added a new dimension of
-				complexity to contemporary international relations. Moreover, the rise
-				of China and other Asian countries, the consolidation of Germany and the
-				EU, the instability of the US, and the effects of capitalism continue to
-				transform the international order, has given us a new dimension of
-				thought when envisioning the future. Given these circumstances, the
-				conference approaches 2023 with the theme of "Ashes of the Old Order:
-				Emergence of the All-Informed Architects of Modern Politics", where we
-				highlight the changing nature of the global political order. Through
-				this event, we aim to explore how this changing order may grant
-				opportunities for new political architects to emerge, and expect your
-				participation in helping this noble cause. <br /> <br /> We sincerely
-				hope that your participation and investment in JOINMUN 2023 will
-				contribute to the advancement of the next generation's understanding and
-				awareness of these pressing global concerns.
-			</p>
-		</section>
-	);
+  return (
+    <section className="container mx-auto mb-28 space-y-8 px-8 sm:px-12 lg:flex lg:items-center lg:justify-center lg:space-x-12 lg:space-y-0 lg:px-16">
+      <div className="flex flex-col items-center justify-center space-y-6 font-headline md:flex-row md:space-x-10 md:space-y-0 lg:flex-col lg:space-x-0 lg:space-y-6">
+        <Image
+          className="h-auto w-48 rounded-full shadow-[0_0_10px_white] md:w-56 lg:min-w-[16rem]"
+          src={fotoPresident}
+          alt="foto president"
+        />
+        <div className="text-center md:space-y-4 md:text-left lg:text-center">
+          <h2 className="text-xl font-bold text-[#f4bb9c] sm:text-2xl md:text-3xl lg:mx-auto lg:max-w-[12ch] lg:text-4xl">
+            Muhammad Riefky Fahreza
+          </h2>
+          <p className="text-base md:text-lg lg:text-xl">
+            President of Jogja International MUN 2023 <br /> Faculty of Social
+            and Political Science, <br /> Universitas Gadjah Mada
+          </p>
+        </div>
+      </div>
+      <p className="mx-auto max-w-prose text-justify text-sm md:text-base lg:text-lg">
+        <span className="mb-2 hidden w-full text-center font-headline text-[4.25rem] lg:block">
+          WELCOMING REMARKS
+        </span>
+        <br />
+        It is beyond my honor to announce the establishment of the 12th
+        conference of Jogja International Model United Nations (‘JOINMUN’), one
+        of the founding MUNs that contains the legacy of great MUNers of
+        Indonesia. The 12th JOINMUN is scheduled to take place from 28 September
+        To 1 October 2023. In this year also, we are committed to providing a
+        platform for insightful discussions and thought-provoking inquiries into
+        current national, regional, and international events. <br /> <br /> We
+        note with regret that these discussions had been pushed back whilst the
+        world faces a multitude of challenges, including high inflation rates,
+        the COVID-19 pandemic, and climate change. The recent developments in
+        Ukraine, with Russia's conquest, have added a new dimension of
+        complexity to contemporary international relations. Moreover, the rise
+        of China and other Asian countries, the consolidation of Germany and the
+        EU, the instability of the US, and the effects of capitalism continue to
+        transform the international order, has given us a new dimension of
+        thought when envisioning the future. Given these circumstances, the
+        conference approaches 2023 with the theme of "Ashes of the Old Order:
+        Emergence of the All-Informed Architects of Modern Politics", where we
+        highlight the changing nature of the global political order. Through
+        this event, we aim to explore how this changing order may grant
+        opportunities for new political architects to emerge, and expect your
+        participation in helping this noble cause. <br /> <br /> We sincerely
+        hope that your participation and investment in JOINMUN 2023 will
+        contribute to the advancement of the next generation's understanding and
+        awareness of these pressing global concerns.
+      </p>
+    </section>
+  );
 };
 export default Remarks;
--- joinmun-web/pages/reveal.js
+++ joinmun-web/pages/reveal.js
@@ -1,90 +1,120 @@
 import { motion } from "framer-motion";
 import Head from "next/head";
+import Image from "next/image";
+import tuguKiri from "../public/reveal/tugu-kiri.svg";
+import tuguKanan from "../public/reveal/tugu-kanan.svg";
 
 const Reveal = () => {
-	return (
-		<section
-			className="min-h-screen"
-			style={{ backgroundImage: "url('./cloud-background.png')" }}
-		>
-			<Head>
-				<title>Reveal | JOINMUN 2023</title>
-			</Head>
-			<motion.div
-				variants={revealVariants}
-				initial="hidden"
-				animate="visible"
-				className="container mx-auto max-w-fit px-4 pt-10 font-headline sm:px-8"
-			>
-				<h2 className="mx-auto mb-3 max-w-fit bg-green px-4 py-2 text-center text-3xl sm:mb-6 sm:px-8 sm:py-4 sm:text-6xl lg:text-7xl">
-					DATE REVEAL
-				</h2>
-				<div className="mb-6 bg-yellow px-[15vw] py-2 text-center text-2xl sm:mb-10 sm:text-5xl md:px-44 lg:text-6xl">
-					<p className="text-red">28 Sept - 1 Oct</p>
-				</div>
-				<div className="grid auto-rows-auto grid-cols-1 gap-6 md:grid-cols-3">
-					<motion.div
-						variants={cardVariants}
-						className="h-40 rounded-lg bg-yellow p-3 sm:p-4"
-					>
-						<div className="bg-brown p-1 text-center text-2xl sm:p-3 sm:text-3xl lg:text-4xl">
-							28 Sept
-						</div>
-					</motion.div>
-					<motion.div
-						variants={cardVariants}
-						className="h-40 rounded-lg bg-yellow p-3 sm:p-4"
-					>
-						<div className="bg-brown p-1 text-center text-2xl sm:p-3 sm:text-3xl lg:text-4xl">
-							29 Sept
-						</div>
-					</motion.div>
-					<motion.div
-						variants={cardVariants}
-						className="h-40 rounded-lg bg-yellow p-3 sm:p-4"
-					>
-						<div className="bg-brown p-1 text-center text-2xl sm:p-3 sm:text-3xl lg:text-4xl">
-							1 Oct
-						</div>
-					</motion.div>
-				</div>
-			</motion.div>
-		</section>
-	);
+  return (
+    <section
+      className="relative min-h-screen overflow-hidden py-20"
+      style={{ backgroundImage: "url('./cloud-background.png')" }}
+    >
+      <Head>
+        <title>Reveal | JOINMUN 2023</title>
+      </Head>
+      <motion.div
+        variants={revealVariants}
+        initial="hidden"
+        animate="visible"
+        className="container mx-auto max-w-fit px-4 pt-10 font-headline sm:px-8"
+      >
+        <h2 className="mx-auto mb-3 max-w-fit bg-green px-4 py-2 text-center text-3xl sm:mb-6 sm:px-8 sm:py-4 sm:text-6xl lg:text-7xl">
+          DATE REVEAL
+        </h2>
+        <div className="mb-6 bg-yellow px-[15vw] py-2 text-center text-2xl sm:mb-10 sm:text-5xl md:px-44 lg:text-6xl">
+          <p className="text-red">28 Sept - 1 Oct</p>
+        </div>
+        <div className="grid auto-rows-auto grid-cols-1 gap-6 md:grid-cols-2">
+          {revealData.map((data) => (
+            <motion.div
+              key={data.date}
+              variants={cardVariants}
+              className="h-80 rounded-lg bg-yellow p-3 sm:p-4"
+            >
+              <div className="bg-brown p-1 text-center text-2xl sm:p-3 sm:text-3xl lg:text-4xl">
+                {data.date}
+              </div>
+            </motion.div>
+          ))}
+        </div>
+      </motion.div>
+
+      {/* Tugu */}
+      <motion.div
+        className="absolute inset-y-0 top-0 my-auto h-4/5 w-full"
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1.5 }}
+      >
+        <motion.div
+          animate={{ x: "calc(-180% + 60vw)" }}
+          transition={{ duration: 2, ease: "easeOut" }}
+          className="absolute left-0 top-0 h-full"
+        >
+          <Image className="h-full w-auto" src={tuguKiri} alt="" />
+        </motion.div>
+        <motion.div
+          animate={{ x: "calc(180% - 60vw)" }}
+          transition={{ duration: 2, ease: "easeOut" }}
+          className="absolute right-0 top-0 h-full"
+        >
+          <Image className="h-full w-auto" src={tuguKanan} alt="" />
+        </motion.div>
+      </motion.div>
+    </section>
+  );
 };
 export default Reveal;
 
 export async function getServerSideProps({ res }) {
-	res.writeHead(302, {
-		Location: "/404",
-	});
-	res.end();
+  // res.writeHead(302, {
+  // 	Location: "/404",
+  // });
+  // res.end();
+  return { props: {} };
 }
 
+const revealData = [
+  {
+    date: "28 Sept",
+  },
+  {
+    date: "29 Sept",
+  },
+  {
+    date: "30 Sept",
+  },
+  {
+    date: "1 Oct",
+  },
+];
+
 const revealVariants = {
-	hidden: {
-		opacity: 0,
-		scale: 0.5,
-	},
-	visible: {
-		opacity: 1,
-		scale: 1,
-		transition: {
-			duration: 1,
-			ease: "easeOut",
-			when: "beforeChildren",
-			staggerChildren: 0.3,
-		},
-	},
+  hidden: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      delay: 1,
+      duration: 1,
+      ease: "easeOut",
+      when: "beforeChildren",
+      staggerChildren: 0.3,
+    },
+  },
 };
 
 const cardVariants = {
-	hidden: {
-		opacity: 0,
-		y: 40,
-	},
-	visible: {
-		opacity: 1,
-		y: 0,
-	},
+  hidden: {
+    opacity: 0,
+    y: 40,
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+  },
 };
